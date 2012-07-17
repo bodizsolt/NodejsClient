af4363030d3590f19ff782ba82f2a0f2383d024b
Zsolt <zsolt@offonstudio.ro>
2012-07-17 23:14:54 +0300

    Basic server and router working, added formidable for uploading files

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 var http = require('http');
+var url = require('url');
 
-function onRequest(request, response){
-  console.log('request received');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
-  response.end();
+function start(route, handle){
+  function onRequest(request, response){
+    var pathname = url.parse(request.url).pathname,
+        postData = "";
+    console.log('Request for '+ pathname +' received');
+
+    request.setEncoding('utf8');
+
+    request.addListener('data',function(postDataChunk){
+      postData += postDataChunk;
+      console.log('Received POST data chunk "' + postDataChunk +'".');
+    });
+
+    request.addListener('end', function(){
+      route(handle, pathname, response, postData);
+    });
+
+    
+  }
+
+  http.createServer(onRequest).listen(8889);
+  console.log('Server has started');
 }
 
-
-http.createServer(onRequest).listen(8889);
-console.log('Server has started');
+exports.start = start;
