76c52ce9dbf603af49a2953c9d7fdf2872194579
Zsolt <zsolt@offonstudio.ro>
2012-07-23 03:28:48 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+Node.js Client
+====================
+
+This is a very simple CRUD application build with Bacbone, Twitter Boostrap on the frontend and Node.js, Express.js and Mongoose(MongoDB ORM) on the backend.
+
+Getting Started
+---------------------
++ Install Node.js and NPM
++ Install mongodb and run mongod
++ Start the server by using
+`node server.js`
++ Open up your browser and point it towards [http://localhost:8080](http://localhost:8080)
++ Profit.
--- public/app/app.js
+++ public/app/app.js
@@ -9,20 +9,19 @@
       App.collection = new flobn.collection.Clients();
       App.addView = new flobn.view.ClientFormView({collection: App.collection});
       App.listView = new flobn.view.ClientListView({collection: App.collection});
-      App.saerchView = new flobn.view.SearchBarView({collection: App.collection});
+      App.searchView = new flobn.view.SearchBarView({collection: App.collection});
       App.collection.fetch();
       App.addView.render();
+      $('#addbtn').click(function(){
+        var client = new flobn.model.Client();
+        App.addView.show(client);
+      });
     }
   };
 
   $(function(){
     
     App.initialize();
-    
-    $('#addbtn').click(function(){
-      var client = new flobn.model.Client();
-      App.addView.show(client);
-    });
   });
 
 
